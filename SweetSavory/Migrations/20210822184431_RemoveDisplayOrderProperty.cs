902c6e3b08e301c869ff11a176618d61145d1900
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-08-22 21:46:06 +0300

    remove display order property from flavors

---
+++ SweetSavory/Migrations/20210822184431_RemoveDisplayOrderProperty.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SweetSavory.Migrations
+{
+    public partial class RemoveDisplayOrderProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DisplayOrder",
+                table: "Treats");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DisplayOrder",
+                table: "Treats",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
